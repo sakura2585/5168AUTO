4f7841db02a30f7e12d0365882afa1fb52244665
sakura2585 <sakura2585@gmail.com>
2026-06-19 18:08:44 +0800

    Save

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from session_guard import LoginResult, LoginStatus
 from workflow_worker import WorkflowThread, WorkflowWorker
 from inventory_workflow import WorkflowResult
 
-_APP_VERSION = "v0.2.10"
+_APP_VERSION = "v0.2.11"
 
 
 def _clamp_geometry(
